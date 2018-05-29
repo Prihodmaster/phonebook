b308e73c621d0c317100b2717b0def88998afff0
prihodmaster <sergeyprihodkozp@gmail.com>
2018-05-29 23:40:09 +0300

    search

--- code/js/containers/phone-list.js
+++ code/js/containers/phone-list.js
@@ -4,22 +4,39 @@ import {connect} from 'react-redux';
 import {select} from '../actions/index';
 import Details from '../containers/details';
 
+let inputField='';
 class PhoneList extends Component{
-    render() {console.log(this.props);
+    constructor(props) {
+        super(props);
+        this.state = {filteredList: this.props.phones};
+    }
+    render() {
         return(
             <div className="ui grid main">
                 <div className="one wide column"></div>
                 <div className="four wide column clients-list">
-                    <input className="search" type="text" placeholder="Search..." />
+                    <input className="search" type="text" placeholder="Search..." value={inputField} onChange={
+                        (e)=>{
+                            inputField = e.target.value;
+                            this.setState({filteredList: this.props.phones.filter(
+                                    function(item){
+                                        if(item.firstName.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1){return true};
+                                        if(item.lastName.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1){return true};
+                                    }
+                                )})
+                        }
+                    } />
                     <ul>
                         {
-                            this.props.phones.map((item, i) => {
-                                return(<li className="list-item" onClick={() => this.props.select (item)} key={i}>
+                            this.state.filteredList.map((item, i) => {
+                                return(
+                                    <li className="list-item" onClick={() => this.props.select (item)} key={i}>
                                     <div className="list-item-img"><img src={item.avatar} alt="фото" /></div>
                                     <div className="list-item-text">
                                         <p>{item.firstName}<br/><span className="conpany">{item.lastName}</span></p>
                                     </div>
-                                </li>)
+                                </li>
+                                )
                         })
                         }
                     </ul>
@@ -37,7 +54,7 @@ function mapStateToProps(state) {
         phones: state.phones
     };
 }
-function matchDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch) {
     return bindActionCreators({select: select, dispatch})
 }
-export default connect(mapStateToProps, matchDispatchToProps)(PhoneList);
+export default connect(mapStateToProps, mapDispatchToProps)(PhoneList);
